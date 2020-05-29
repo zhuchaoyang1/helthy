1d9fa9ae0577eb415de15a9a4f6f5b80eca9db68
zhuchaoyang <ZCYSXJ111.Q>
2020-05-29 22:42:45 +0800

    本地版本

--- heathy-server-zuul/src/main/java/com/zcy/cn/HeathyServerZuulApplication.java
+++ heathy-server-zuul/src/main/java/com/zcy/cn/HeathyServerZuulApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
